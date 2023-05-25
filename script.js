b1b5ec9cd6c34831866eb09d1dc626c6f8e92cda
Siar Wahidi <siar.wahidi@gmail.com>
2023-05-24 22:46:13 -0400

    completed valid anagram problem

--- script.js
+++ script.js
@@ -25,4 +25,48 @@ const containsDuplicate = (nums) => {
 
   return false;
 };
-console.log(containsDuplicate([1, 2, 3, 1]));
+// console.log(containsDuplicate([1, 2, 3, 1]));
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// Valid Anagram
+
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a
+// different word or phrase, typically using all the original letters exactly once.
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+
+// Input: s = "rat", t = "car"
+// Output: false
+
+const isAnagram = (s, t) => {
+  if (s.length !== t.length) return false;
+
+  let map = {};
+
+  for (let i = 0; i < s.length; i++) {
+    if (!map[s[i]]) {
+      map[s[i]] = 1;
+    } else {
+      map[s[i]]++;
+    }
+  }
+
+  for (let i = 0; i < t.length; i++) {
+    if (map[t[i]]) {
+      map[t[i]]--;
+    }
+  }
+
+  for (const letter in map) {
+    if (map[letter] > 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+console.log(isAnagram("anagram", "nagaram"));
