e5ef0786f13e7ef17a668cb002faf93860dd5392
Siar Wahidi <siar.wahidi@gmail.com>
2023-06-07 15:43:56 -0400

    Solved problem Top K Frequent Elements

--- script.js
+++ script.js
@@ -140,4 +140,44 @@ const groupAnagrams = (strs) => {
 
   return Object.values(map);
 };
-console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+// console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// Top K Frequent Elements
+
+// Given an integer array nums and an integer k, return the k most
+// frequent elements. You may return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [1,1,1,2,2,3], k = 2
+// Output: [1,2]
+
+// Example 2:
+
+// Input: nums = [1], k = 1
+// Output: [1]
+
+const topKFrequent = (nums, k) => {
+  let map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (!map[nums[i]]) {
+      map[nums[i]] = 1;
+    } else {
+      map[nums[i]]++;
+    }
+  }
+
+  let sorted = Object.entries(map).sort((a, b) => b[1] - a[1]);
+
+  let result = [];
+
+  for (let i = 0; i < k; i++) {
+    result.push(sorted[i][0]);
+  }
+
+  return result;
+};
+console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
