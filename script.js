4fdbacef3b901f9044ecd48e438c70690f0f9395
Siar Wahidi <siar.wahidi@gmail.com>
2023-06-12 14:33:43 -0400

    Solved problem: Valid Sudoku

--- script.js
+++ script.js
@@ -223,4 +223,59 @@ const productExceptSelf = (nums) => {
 
   return reverseArray;
 };
-console.log(productExceptSelf([1, 2, 3, 4]));
+// console.log(productExceptSelf([1, 2, 3, 4]));
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// Valid Sudoku
+
+// Determine if a 9 x 9 Sudoku board is valid.
+// Only the filled cells need to be validated according to the following rules:
+
+// Each row must contain the digits 1-9 without repetition.
+// Each column must contain the digits 1-9 without repetition.
+// Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+// Note:
+
+// A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+// Only the filled cells need to be validated according to the mentioned rules.
+
+const isValidSudoku = (board) => {
+  let row = {};
+  let col = {};
+  let box = {};
+
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      let value = board[row][col];
+      if (value !== ".") {
+        let boxIdx = Math.floor(row / 3) * 3 + Math.floor(col / 3);
+        if (
+          row[`${row}-${value}`] ||
+          col[`${col}-${value}`] ||
+          box[`${boxIdx}-${value}`]
+        ) {
+          return false;
+        }
+        row[`${row}-${value}`] = true;
+        col[`${col}-${value}`] = true;
+        box[`${boxIdx}-${value}`] = true;
+      }
+    }
+  }
+
+  return true;
+};
+console.log(
+  isValidSudoku([
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+  ])
+);
