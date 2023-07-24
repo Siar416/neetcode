d2121706f51731d96dfb38ef6888839c6d8d7629
Siar Wahidi <siar.wahidi@gmail.com>
2023-07-24 13:59:17 -0400

    Solved Hackerrank problem Grading Students

--- script.js
+++ script.js
@@ -373,7 +373,7 @@ const threeSum = (nums) => {
   }
   return result;
 };
-console.log(threeSum([-1, 0, 1, 2, -1, -4]));
+// console.log(threeSum([-1, 0, 1, 2, -1, -4]));
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -404,4 +404,43 @@ function runningTime(arr) {
   }
   return shifts;
 }
-console.log(runningTime([2, 1, 3, 1, 2]));
+// console.log(runningTime([2, 1, 3, 1, 2]));
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// HackerLand University has the following grading policy:
+
+// Every student receives a  in the inclusive range from  to .
+// Any  less than  is a failing grade.
+// Sam is a professor at the university and likes to round each student's  according to these rules:
+
+// If the difference between the  and the next multiple of  is less than , round  up to the next multiple of .
+// If the value of  is less than , no rounding occurs as the result will still be a failing grade.
+
+// input:
+// 73
+// 67
+// 38
+// 33
+
+// output
+// 75
+// 67
+// 40
+// 33
+
+function gradingStudents(grades) {
+  let resultGrades = [];
+  let gradeRounded = 0;
+
+  for (let i = 0; i < grades.length; i++) {
+    gradeRounded = Math.ceil(grades[i] / 5) * 5;
+    if (gradeRounded - grades[i] < 3 && grades[i] >= 38) {
+      resultGrades.push(gradeRounded);
+    } else {
+      resultGrades.push(grades[i]);
+    }
+  }
+  return resultGrades;
+}
+console.log(gradingStudents([73, 67, 38, 33]));
